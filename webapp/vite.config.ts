eddc5e67a1973ae49730bcd1ce1e6a4cf3fa47cf
luobochuanqi <luobochuanqi@qq.com>
2026-04-09 23:52:06 +0800

    feat: try to fix github pages

--- webapp/vite.config.ts
+++ webapp/vite.config.ts
@@ -4,6 +4,7 @@ import tailwindcss from '@tailwindcss/vite'
 import path from 'path'
 
 export default defineConfig({
+  base: '/interactive-bitwise-guide/',  // GitHub Pages 子目录路径
   plugins: [vue(), tailwindcss()],
   resolve: {
     alias: {
